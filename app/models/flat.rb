4a5cd911494efcd6b858dfb1539473e70ed27706
tommasobicocchi <tommaso.bicocchi@gmail.com>
2019-12-02 16:51:53 +0100

    fake commit

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -2,4 +2,9 @@ class Flat < ApplicationRecord
   belongs_to :user
   has_many :bookings
   has_many :reviews, through: :bookings
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :daily_price, presence: true
+  validates :address, presence: true
 end
+
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :booking
+  validates :rating, presence: true
+  validates :description, presence: true
+  validates_inclusion_of :rating, :in => [0, 1, 2, 3, 4, 5]
 end
