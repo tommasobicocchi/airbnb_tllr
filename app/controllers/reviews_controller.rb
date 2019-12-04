7a49b4be99f21f3f8d9ef7ed7331220fdef4bc16
Leonardo Dalla Via <dallavia.leonardo@gmail.com>
2019-12-04 14:49:01 +0100

    More buttons and paths

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,11 +9,13 @@ class ReviewsController < ApplicationController
   def new
     @booking = Booking.find(params[:booking_id])
     @review = Review.new
+    authorize @review
   end
 
   def create
     @booking = Booking.find(params[:booking_id])
     @review = Review.new(review_params)
+    authorize @review
     @review.booking = @booking
     @review.rating = review_params["rating"].to_i
     if @review.save
